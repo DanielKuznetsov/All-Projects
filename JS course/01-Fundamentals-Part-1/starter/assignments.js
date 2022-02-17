652491fdecaf9bc706fccac0526ffc477e1c36d9
Daniel Kuznetsov <85589967+DanilKuznet1@users.noreply.github.com>
2022-02-17 15:18:57 -0800

    learned about variables, different data types, basic operators

---
+++ JS course/01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,42 @@
+// let population = 3000;
+// console.log(population / 2);
+
+// population++;
+
+// console.log(population);
+
+// let populationFinland = 6000000;
+// console.log(population > populationFinland);
+
+// const country = "USA";
+// const continent = "North America";
+// language = "english";
+
+// const description =
+//   country +
+//   " is in " +
+//   continent +
+//   " , and its " +
+//   population +
+//   " speak " +
+//   language;
+
+// console.log(description);
+
+//mark
+const weightMark = 78;
+const heightMark = 1.69;
+
+const bmiMark = weightMark / heightMark ** 2;
+
+//john
+const weightJohn = 92;
+const heightJohn = 1.95;
+
+const bmiJohn = weightJohn / heightJohn ** 2;
+
+//checking whose BMI is higher
+const markHigherBMI = bmiMark > bmiJohn;
+
+//result to the console
+console.log(bmiMark, bmiJohn, markHigherBMI);
