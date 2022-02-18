a9d8e4c92d1f1dadf5cc67b7b97b3984e250aebb
Daniel Kuznetsov <85589967+DanilKuznet1@users.noreply.github.com>
2022-02-18 14:00:06 -0800

    learned how to use logic operators

--- JS course/01-Fundamentals-Part-1/starter/assignments.js
+++ JS course/01-Fundamentals-Part-1/starter/assignments.js
@@ -1,9 +1,24 @@
-let population = 300000000;
+// let population = 300000000;
 
-const country = "USA";
-const continent = "North America";
-language = "english";
+// const country = "USA";
+// const continent = "North America";
+// language = "english";
 
-const description = `${country} is in ${continent}, and its population is ${population} that speaks ${language}`;
+// const description = `${country} is in ${continent}, and its population is ${population} that speaks ${language}`;
 
-console.log(description);
+// console.log(description);
+
+// //type conversion
+// const inputYear = "1998";
+// console.log(Number(inputYear) + 18);
+
+// //type conversion in prompt
+// const age = 22;
+// if (Number(age) === 22) console.log(`you just turned 22`);
+
+// const numberPrompt = prompt(`What is your favorite number?`);
+// if (Number(numberPrompt) >= 10) {
+//   console.log("The number is above or equal to 10");
+// } else {
+//   console.log(`The number is less than 10`);
+// }
