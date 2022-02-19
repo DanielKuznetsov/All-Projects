cc1aa38373ab5a2fe4aad998a4e8bca9ee796cdd
Daniel Kuznetsov <85589967+DanilKuznet1@users.noreply.github.com>
2022-02-19 12:02:27 -0800

    practice switch conditions

--- JS course/01-Fundamentals-Part-1/starter/assignments.js
+++ JS course/01-Fundamentals-Part-1/starter/assignments.js
@@ -12,13 +12,37 @@
 // const inputYear = "1998";
 // console.log(Number(inputYear) + 18);
 
-// //type conversion in prompt
+// type conversion in prompt
 // const age = 22;
 // if (Number(age) === 22) console.log(`you just turned 22`);
 
-// const numberPrompt = prompt(`What is your favorite number?`);
-// if (Number(numberPrompt) >= 10) {
+// const numberPrompt = Number(prompt(`What is your favorite number?`));
+// if (numberPrompt >= 10) {
 //   console.log("The number is above or equal to 10");
 // } else {
 //   console.log(`The number is less than 10`);
 // }
+
+//practicing switch conditions
+const language = "english";
+
+switch (language) {
+  case `chinese`:
+  case `mandarine`:
+    console.log(`Most number of native speakers!`);
+    break;
+  case `spanish`:
+    console.log(`2nd place in number of native speakers'`);
+    break;
+  case `english`:
+    console.log(`3rd place`);
+    break;
+  case `hindi`:
+    console.log`Number 4`;
+    break;
+  case `arabic`:
+    console.log(`5th most spoken langauge`);
+    break;
+  default:
+    console.log(`Great language too! :D`);
+}
---
+++ JS course/01-Fundamentals-Part-1/starter/challenge1.js
@@ -0,0 +1,35 @@
+// // CHALLENGE USING IF/ELSE STATEMENT
+// //mark
+// const weightMark = 78;
+// const heightMark = 1.69;
+
+// const bmiMark = weightMark / heightMark ** 2;
+
+// //john
+// const weightJohn = 92;
+// const heightJohn = 1.95;
+
+// const bmiJohn = weightJohn / heightJohn ** 2;
+
+// //checking whose BMI is higher
+// const markHigherBMI = bmiMark > bmiJohn;
+
+// let whoseScoreHigher;
+// if (bmiMark > bmiJohn) {
+//   whoseScoreHigher = `Mark has a higher BMI score of ${bmiMark}`;
+// } else {
+//   whoseScoreHigher = `John has a higher BMI score of ${bmiJohn}`;
+// }
+
+// console.log(whoseScoreHigher);
+
+// //CHALLENGE USING PROMPT AND LOGIC OPERATIRS
+// const language = prompt(`What language does your country speak?`);
+// const population = prompt(`What population of your country?`);
+// const isIsland = prompt(`Is your country an island, YES/NO?`);
+
+// if (language === `english` && population <= 50000000 && isIsland == `no`) {
+//   console.log(`You might like Portugal :)`);
+// } else {
+//   console.log(`Portugal doesn't meet your criteria ;(`);
+// }
