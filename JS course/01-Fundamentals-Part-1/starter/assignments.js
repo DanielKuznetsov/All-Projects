de554c4db853a7249c7fabf1ecfee37e0368a551
Daniel Kuznetsov <85589967+DanilKuznet1@users.noreply.github.com>
2022-02-19 14:35:04 -0800

    practiced conditional/ternary operator
    which can be used in simple expressions

--- JS course/01-Fundamentals-Part-1/starter/assignments.js
+++ JS course/01-Fundamentals-Part-1/starter/assignments.js
@@ -24,25 +24,36 @@
 // }
 
 //practicing switch conditions
-const language = "english";
+// const language = "english";
 
-switch (language) {
-  case `chinese`:
-  case `mandarine`:
-    console.log(`Most number of native speakers!`);
-    break;
-  case `spanish`:
-    console.log(`2nd place in number of native speakers'`);
-    break;
-  case `english`:
-    console.log(`3rd place`);
-    break;
-  case `hindi`:
-    console.log`Number 4`;
-    break;
-  case `arabic`:
-    console.log(`5th most spoken langauge`);
-    break;
-  default:
-    console.log(`Great language too! :D`);
-}
+// switch (language) {
+//   case `chinese`:
+//   case `mandarine`:
+//     console.log(`Most number of native speakers!`);
+//     break;
+//   case `spanish`:
+//     console.log(`2nd place in number of native speakers'`);
+//     break;
+//   case `english`:
+//     console.log(`3rd place`);
+//     break;
+//   case `hindi`:
+//     console.log`Number 4`;
+//     break;
+//   case `arabic`:
+//     console.log(`5th most spoken langauge`);
+//     break;
+//   default:
+//     console.log(`Great language too! :D`);
+// }
+
+//practicing conditional(ternary) operator
+population = Number(prompt(`What is your country's pupolation, in millions?`));
+
+console.log(
+  `${
+    population > 33
+      ? "Portugal's population is above average"
+      : "Portugal's population is below average"
+  }`
+);
