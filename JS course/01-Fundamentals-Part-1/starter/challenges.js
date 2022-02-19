13cb05896da4c1f2091857cbe87c6e67d8fc9af5
Daniel Kuznetsov <85589967+DanilKuznet1@users.noreply.github.com>
2022-02-18 16:11:15 -0800

    finished challenged using if/else and logic operators

--- JS course/01-Fundamentals-Part-1/starter/challenges.js
+++ JS course/01-Fundamentals-Part-1/starter/challenges.js
@@ -23,13 +23,60 @@
 
 // console.log(whoseScoreHigher);
 
-//CHALLENGE USING PROMPT AND LOGIC OPERATIRS
-const language = prompt(`What language does your country speak?`);
-const population = prompt(`What population of your country?`);
-const isIsland = prompt(`Is your country an island, YES/NO?`);
+// //CHALLENGE USING PROMPT AND LOGIC OPERATIRS
+// const language = prompt(`What language does your country speak?`);
+// const population = prompt(`What population of your country?`);
+// const isIsland = prompt(`Is your country an island, YES/NO?`);
 
-if (language === `english` && population <= 50000000 && isIsland == `no`) {
-  console.log(`You might like Portugal :)`);
+// if (language === `english` && population <= 50000000 && isIsland == `no`) {
+//   console.log(`You might like Portugal :)`);
+// } else {
+//   console.log(`Portugal doesn't meet your criteria ;(`);
+// }
+
+//CHALLENGE USING IF/ELSE AND LOGIC OPERATORS
+const dolphinScoreOne = 96;
+const dolphinScoreTwo = 108;
+const dolphinScoreThree = 89;
+
+const coalaScoreOne = 88;
+const coalaScoreTwo = 91;
+const coalaScoreThree = 110;
+
+//calculating an average score
+const dolphinAverage =
+  (dolphinScoreOne + dolphinScoreTwo + dolphinScoreThree) / 3;
+const coalaAverage = (coalaScoreOne + coalaScoreTwo + coalaScoreThree) / 3;
+console.log(
+  `Dolphins' average score is ${dolphinAverage} and Coala's is ${coalaAverage}`
+);
+
+//checking for who has got a higher average score
+/////////////////////////////////////////////
+// if (dolphinAverage > coalaAverage) {
+//   console.log(
+//     `Dolphins' team has a higher average score, therefore it makes them a winner`
+//   );
+// } else if (dolphinAverage < coalaAverage) {
+//   console.log(
+//     `Coala's team has a higher average score, therefore it makes them a winner`
+//   );
+// } else {
+//   console.log(`Their scores are the same and there is no winners`);
+// }
+
+//checking for a minimum of 100 average score, finding a winner and draw
+//////////////////////////////////////////////////////////
+if ((dolphinAverage && coalaAverage) >= 100 && dolphinAverage > coalaAverage) {
+  console.log(
+    `Dolphins' team has a higher average score, therefore it makes them a winner`
+  );
+} else if (dolphinAverage < coalaAverage) {
+  console.log(
+    `Coala's team has a higher average score, therefore it makes them a winner`
+  );
+} else if (dolphinAverage === coalaAverage) {
+  console.log(`Their scores are the same and there is no winners`);
 } else {
-  console.log(`Portugal doesn't meet your criteria ;(`);
+  console.log(`Average score must be a minimum of a 100`);
 }
