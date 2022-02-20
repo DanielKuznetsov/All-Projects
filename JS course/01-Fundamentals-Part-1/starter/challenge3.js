469344e1f4d9fcda22b23c9a247dd7ad8e772aa6
Daniel Kuznetsov <85589967+DanilKuznet1@users.noreply.github.com>
2022-02-19 16:29:18 -0800

    conditional operator in a function

--- JS course/01-Fundamentals-Part-1/starter/challenge3.js
+++ JS course/01-Fundamentals-Part-1/starter/challenge3.js
@@ -1,8 +1,9 @@
-const bill = 430;
+// //conditional operator challege
+// const bill = 430;
 
-const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
-console.log(
-  `The bill was ${bill}, the tip was ${tip}, and the total value is ${
-    tip + bill
-  }`
-);
+// const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// console.log(
+//   `The bill was ${bill}, the tip was ${tip}, and the total value is ${
+//     tip + bill
+//   }`
+// );
---
+++ JS course/02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,26 @@
+"use strict"; //more secire code, avoiding accidental errors
+
+// const apples = Number(prompt(`How many apples do you have?`));
+// const oranges = Number(prompt(`How many oranges do you have?`));
+
+// function fruitPrecessor() {
+//   const juice = `Juice with ${apples} ${
+//     apples === 1 ? "apple" : "apples"
+//   } and ${oranges} ${oranges === 1 ? "orange" : "oranges"}.`;
+//   return juice;
+// }
+
+// console.log(fruitPrecessor(apples, oranges));
+
+//declaring variable through conditional operator in a function
+const country = prompt(`What country do you live in?`);
+const population = prompt(`What population of ${country}, in millions?`);
+const capital = prompt(`What is capital of ${country}?`);
+
+function describeCountry() {
+  console.log(
+    `${country} has a population of ${population} million people and its capital city is ${capital}`
+  );
+}
+
+describeCountry(country, population, capital);
