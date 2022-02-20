68e6dd82ed589eb872ae27f40dad540506be3f79
Daniel Kuznetsov <85589967+DanilKuznet1@users.noreply.github.com>
2022-02-20 11:51:07 -0800

    learned about declaration and expesion functions

--- JS course/02-Fundamentals-Part-2/starter/script.js
+++ JS course/02-Fundamentals-Part-2/starter/script.js
@@ -1,26 +1,19 @@
 "use strict"; //more secire code, avoiding accidental errors
 
-// const apples = Number(prompt(`How many apples do you have?`));
-// const oranges = Number(prompt(`How many oranges do you have?`));
-
-// function fruitPrecessor() {
-//   const juice = `Juice with ${apples} ${
-//     apples === 1 ? "apple" : "apples"
-//   } and ${oranges} ${oranges === 1 ? "orange" : "oranges"}.`;
-//   return juice;
-// }
-
-// console.log(fruitPrecessor(apples, oranges));
-
-//declaring variable through conditional operator in a function
-const country = prompt(`What country do you live in?`);
-const population = prompt(`What population of ${country}, in millions?`);
-const capital = prompt(`What is capital of ${country}?`);
-
-function describeCountry() {
-  console.log(
-    `${country} has a population of ${population} million people and its capital city is ${capital}`
-  );
+//////////////////////////////////
+//declaration function
+function calcPercentage1(population) {
+  return (population / 7900) * 100;
 }
 
-describeCountry(country, population, capital);
+const percentageOfWorld1 = calcPercentage1(150);
+console.log(percentageOfWorld1);
+
+//////////////////////////////////
+//expression function
+const calcPercentage2 = function (population) {
+  return (population / 7900) * 100;
+};
+
+const percentageOfWorld2 = calcPercentage2(250);
+console.log(percentageOfWorld2);
