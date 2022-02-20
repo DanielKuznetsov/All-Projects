3b4dab2883a6207990b563eac05d608262296fe4
Daniel Kuznetsov <85589967+DanilKuznet1@users.noreply.github.com>
2022-02-20 12:26:29 -0800

    learned arrow function
    made an example using conditional operator

--- JS course/02-Fundamentals-Part-2/starter/script.js
+++ JS course/02-Fundamentals-Part-2/starter/script.js
@@ -17,3 +17,35 @@ const calcPercentage2 = function (population) {
 
 const percentageOfWorld2 = calcPercentage2(250);
 console.log(percentageOfWorld2);
+
+//////////////////////////////////
+// arrow function
+const calcPercentageOfWorld3 = (population, totalWorldPopulation) => {
+  return (population / totalWorldPopulation) * 100;
+};
+
+const percentageOfWorld3 = calcPercentageOfWorld3(250, 7000);
+
+console.log(percentageOfWorld3);
+
+//////////////////////////////////
+// arrow function with a conditional operator
+const calcAge1 = (currentYear, birthYear) => currentYear - birthYear;
+
+const age1 = calcAge1(2022, 1998);
+console.log(age1);
+
+const yearsUntilRetirement = (
+  currentYear,
+  birthYear,
+  retirementAge,
+  firstName
+) => {
+  const age = currentYear - birthYear;
+  const retirement = retirementAge - age;
+  return `${firstName} is ${age} years old and has ${retirement} ${
+    retirement > 0 ? "years until retirement" : "years since he retired"
+  }`;
+};
+
+console.log(yearsUntilRetirement(2022, 1998, 65, "Daniel"));
