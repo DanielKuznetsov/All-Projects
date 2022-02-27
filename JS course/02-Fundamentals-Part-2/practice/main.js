aae0689b0572bc3d5d48bf0082ec9cb3810a5cdf
Daniel Kuznetsov <85589967+DanilKuznet1@users.noreply.github.com>
2022-02-27 11:51:24 -0800

    sum of 2 arrays

--- JS course/02-Fundamentals-Part-2/practice/main.js
+++ JS course/02-Fundamentals-Part-2/practice/main.js
@@ -1,25 +1,41 @@
 //EX1
 
-const birthYears = [1998, 2007, 1976, 1991, 1944];
-const ages = [];
-let average;
+// const birthYears = [1998, 2007, 1976, 1991, 1944];
+// const ages = [];
+// let average;
 
-const calcAge = function (currentYear) {
-  for (let i = 0; i < birthYears.length; i++) {
-    const age = currentYear - birthYears[i];
-    ages.push(age);
+// const calcAge = function (currentYear) {
+//   for (let i = 0; i < birthYears.length; i++) {
+//     const age = currentYear - birthYears[i];
+//     ages.push(age);
+//   }
+//   return ages;
+// };
+
+// const calcAverage = function (arr) {
+//   let sum = 0;
+//   for (y = 0; y < arr.length; y++) {
+//     sum += arr[y];
+//     average = sum / arr.length;
+//   }
+//   return average;
+// };
+
+// console.log(calcAge(2022));
+// console.log(calcAverage(ages));
+
+//EX2
+
+const arr_1 = [3, 5, 22, 5, 7, 2, 45, 75, 89, 21, 2]; // --> 276
+const arr_2 = [9, 2, 42, 55, 71, 22, 4, 5, 90, 25, 26]; // --> 351
+
+const calcSum = function (arr) {
+  sum = 0;
+  for (i = 0; i < arr.length; i++) {
+    sum += arr[i];
   }
-  return ages;
+  return sum;
 };
 
-const calcAverage = function (arr) {
-  let sum = 0;
-  for (y = 0; y < arr.length; y++) {
-    sum += arr[y];
-    average = sum / arr.length;
-  }
-  return average;
-};
-
-console.log(calcAge(2022));
-console.log(calcAverage(ages));
+const totalSum = calcSum(arr_1) + calcSum(arr_2);
+console.log(totalSum);
