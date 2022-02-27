1ac6c9d3346a2fafd37c59df985a30ee6d17bff6
Daniel Kuznetsov <85589967+DanilKuznet1@users.noreply.github.com>
2022-02-27 12:48:34 -0800

    reversed array using for

--- JS course/02-Fundamentals-Part-2/practice/main.js
+++ JS course/02-Fundamentals-Part-2/practice/main.js
@@ -26,16 +26,29 @@
 
 //EX2
 
-const arr_1 = [3, 5, 22, 5, 7, 2, 45, 75, 89, 21, 2]; // --> 276
-const arr_2 = [9, 2, 42, 55, 71, 22, 4, 5, 90, 25, 26]; // --> 351
+// const arr_1 = [3, 5, 22, 5, 7, 2, 45, 75, 89, 21, 2]; // --> 276
+// const arr_2 = [9, 2, 42, 55, 71, 22, 4, 5, 90, 25, 26]; // --> 351
 
-const calcSum = function (arr) {
-  sum = 0;
-  for (i = 0; i < arr.length; i++) {
-    sum += arr[i];
+// const calcSum = function (arr) {
+//   sum = 0;
+//   for (i = 0; i < arr.length; i++) {
+//     sum += arr[i];
+//   }
+//   return sum;
+// };
+
+// const totalSum = calcSum(arr_1) + calcSum(arr_2);
+// console.log(totalSum);
+
+//EX3
+const arr = [43, "what", 9, true, "cannot", false, "be", 3, true];
+const reversed = [];
+
+const reversedArr = function (arr) {
+  for (i = arr.length - 1; i >= 0; i--) {
+    reversed.push(arr[i]);
   }
-  return sum;
+  return reversed;
 };
 
-const totalSum = calcSum(arr_1) + calcSum(arr_2);
-console.log(totalSum);
+console.log(reversedArr(arr));
