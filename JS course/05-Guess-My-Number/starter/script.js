f9d644f174f8d4425ec8a6b1e52a8002b2016d87
Daniel Kuznetsov <85589967+DanilKuznet1@users.noreply.github.com>
2022-02-28 21:01:40 -0800

    added styles to body, guess number(width and transition) in JS

--- JS course/05-Guess-My-Number/starter/script.js
+++ JS course/05-Guess-My-Number/starter/script.js
@@ -6,16 +6,27 @@ document.querySelector('.number').textContent = secretNumber;
 
 let score = 20;
 
-//ewcord value of input into the button
+//record value of input into the button
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
   //function for checking the guessed number
+
+  //when there is no input
   if (!guess) {
     document.querySelector('.message').textContent = '🚫 No Number!';
+
+    //when player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
-    location.reload();
+
+    //changing body color when guess is correct
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    //changing width
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').style.transition = 'all .25s';
+
+    //when guess is higher
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📈 Too High!';
@@ -25,6 +36,8 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = '💣 You lost the game!';
       document.querySelector('.score').textContent = 0;
     }
+
+    //when guess is lower
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📉 Too Low!';
@@ -35,4 +48,9 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
+});
+
+//reload the page
+document.querySelector('.again').addEventListener('click', function () {
+  location.reload();
 });
