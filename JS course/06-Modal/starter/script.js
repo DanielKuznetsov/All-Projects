84783b8ef5bfb8cb7d2d5824ea558a886b90004d
Daniel Kuznetsov <85589967+DanilKuznet1@users.noreply.github.com>
2022-03-02 16:04:58 -0800

    adding class and removing them to open/close modal
    same class name can be looped through and do not forget use All when selecting them

--- JS course/06-Modal/starter/script.js
+++ JS course/06-Modal/starter/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+//function to open modal
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+//function to close modal
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+//looping over the same class elements and open modal
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+//close modal
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
