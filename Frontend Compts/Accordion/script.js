09bc2aa7d601d878b12aacbfb126ac1cda1c359d
Daniel Kuznetsov <85589967+DanilKuznet1@users.noreply.github.com>
2022-03-07 21:59:47 -0800

    rotating icon of corresponding card

--- Frontend Compts/Accordion/script.js
+++ Frontend Compts/Accordion/script.js
@@ -2,8 +2,12 @@ const card = document.querySelectorAll(".card");
 
 for (let i = 0; i < card.length; i++) {
   card[i].addEventListener("click", function () {
+    //open modal that corresponds to the card we click on
     cardBody = document.querySelectorAll(".card-body");
-
     cardBody[i].classList.toggle(`card-body--${i}`);
+
+    //rotating icon of corresponding card
+    cardIcon = document.querySelectorAll(".card-icon");
+    cardIcon[i].classList.toggle(`moveable`);
   });
 }
