21a84f288c51ece8a3d5aac0258dbd8ac5524d5e
Daniel Kuznetsov <85589967+DanilKuznet1@users.noreply.github.com>
2022-03-11 17:21:37 -0800

    learned about desctructuring objects

--- JS course/09-Data-Structures-Operators/starter/script.js
+++ JS course/09-Data-Structures-Operators/starter/script.js
@@ -30,12 +30,59 @@ const restaurant = {
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
+
+  orderDelivery: function ({ starterIndex, mainIndex, time, address }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
 };
 
+restaurant.orderDelivery({
+  time: '22:30',
+  address: '333 First St',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+//OBJECT DESCTRUCTURING
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+//giving const variables new names (when dealing with 3rd party data)
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+
+//Default values
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+//Mutating variables
+let a = 111;
+let b = 999;
+const obj = {
+  a: 23,
+  b: 7,
+  c: 14,
+};
+
+({ a, b } = obj);
+console.log(a, b);
+
+//Nested objects
+const {
+  fri: { open: o, close: c },
+} = openingHours;
+console.log(o, c);
+
+//ARRAY DESCTRUCTURING
 const arr = [2, 3, 4];
-const a = arr[0];
-const b = arr[1];
-const c = arr[2];
+// const a = arr[0];
+// const b = arr[1];
+// const c = arr[2];
 
 const [x, y, z] = arr;
 console.log(x, y, z);
@@ -58,9 +105,9 @@ const [q, w] = j;
 console.log(q, w);
 
 //or using destructuring
-const [p, , [o, i]] = nested;
-console.log(o, i);
+const [p, , [v, i]] = nested;
+console.log(v, i);
 
-//Defaukt values
+//Default values
 const [t = 1, e = 1, u = 1] = [8, 5];
 console.log(t, e, u);
