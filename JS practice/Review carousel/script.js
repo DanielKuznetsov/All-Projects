8ac46d0e46a9b3d865fcd9af039f929153c800ba
Daniel Kuznetsov <85589967+DanilKuznet1@users.noreply.github.com>
2022-03-23 10:56:05 -0700

    added comments to my code to remember my steps in the future

--- JS practice/Review carousel/script.js
+++ JS practice/Review carousel/script.js
@@ -1,12 +1,13 @@
+//Assigning variables to items we are manipulating
 const cards = document.querySelectorAll(".card");
 const btns = document.querySelectorAll("span");
 const cardTitles = document.querySelectorAll(".card__title");
 const cardTexts = document.querySelectorAll(".card__text");
 const imgs = document.querySelectorAll(".img");
 
+//Loop to un-select all card from all btns and effects
 btns.forEach((button) => {
   button.addEventListener("click", () => {
-    //un-select all items
     cards.forEach((card) => {
       card.classList.remove("active");
     });
@@ -16,18 +17,22 @@ btns.forEach((button) => {
   });
 });
 
-//Selecting btns with a corresponding [i] of card, title, text
+//Loop to SELECT each card, title, text, img according to [i] in the array
 for (let i = 0; i < btns.length; i++) {
+  //when clicking the btn
   btns[i].addEventListener("click", () => {
+    //card will be display: block
     cards[i].classList.add("active");
+    //title and text will receive values in the corresponding classList.add("X")
     cardTitles[i].classList.add("transity");
     cardTexts[i].classList.add("transity-text");
+    //btns and img will obtain their animation effect
     btns[i].classList.add("span-active");
     imgs[i].classList.add("img-anmt");
   });
 }
 
-//
+//Loop to make the title, text, img BLANK separately
 cardTitles.forEach((title) => {
   title.classList.add("opacity");
   cardTexts.forEach((text) => {
