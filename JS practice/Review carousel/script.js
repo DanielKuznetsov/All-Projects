bb3e2f45f0a06701317e9a4eef5707d138f40d74
Daniel Kuznetsov <85589967+DanilKuznet1@users.noreply.github.com>
2022-03-23 10:47:32 -0700

    title, text, img animation is done

--- JS practice/Review carousel/script.js
+++ JS practice/Review carousel/script.js
@@ -1,5 +1,8 @@
 const cards = document.querySelectorAll(".card");
 const btns = document.querySelectorAll("span");
+const cardTitles = document.querySelectorAll(".card__title");
+const cardTexts = document.querySelectorAll(".card__text");
+const imgs = document.querySelectorAll(".img");
 
 btns.forEach((button) => {
   button.addEventListener("click", () => {
@@ -8,13 +11,29 @@ btns.forEach((button) => {
       card.classList.remove("active");
     });
     btns.forEach((btn) => {
-      btn.classList.remove("active");
+      btn.classList.remove("span-active");
     });
   });
 });
 
+//Selecting btns with a corresponding [i] of card, title, text
 for (let i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", () => {
     cards[i].classList.add("active");
+    cardTitles[i].classList.add("transity");
+    cardTexts[i].classList.add("transity-text");
+    btns[i].classList.add("span-active");
+    imgs[i].classList.add("img-anmt");
   });
 }
+
+//
+cardTitles.forEach((title) => {
+  title.classList.add("opacity");
+  cardTexts.forEach((text) => {
+    text.classList.add("opacity");
+  });
+  imgs.forEach((img) => {
+    img.classList.add("img-opacity");
+  });
+});
