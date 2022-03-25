448ab5340429525b149d4369b5f934a93e4258a7
Daniel Kuznetsov <85589967+DanilKuznet1@users.noreply.github.com>
2022-03-25 09:08:15 -0700

    slider #2
    works because of the transform: translateY(); property on the parent object

--- JS practice/Slider #2/script.js
+++ JS practice/Slider #2/script.js
@@ -1,1 +1,27 @@
-const 
+const boxSlider = document.querySelector(".box");
+const btnNext = document.querySelector(".next");
+const btnPrev = document.querySelector(".prev");
+const slideIndexLength = document.querySelectorAll(".card").length;
+
+let currentSlideIndex = 0;
+
+btnNext.addEventListener("click", () => slider("next"));
+btnPrev.addEventListener("click", () => slider("prev"));
+
+const slider = function (direction) {
+  if (direction === "next") {
+    currentSlideIndex++;
+    if (currentSlideIndex > slideIndexLength - 1) {
+      currentSlideIndex = 0;
+    }
+  } else if (direction === "prev") {
+    currentSlideIndex--;
+    if (currentSlideIndex < 0) {
+      currentSlideIndex = slideIndexLength - 1;
+    }
+  }
+
+  //If a container has children above its' width
+  //and height, we can move children
+  boxSlider.style.transform = `translateY(-${currentSlideIndex * 100}%)`;
+};
