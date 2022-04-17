073f3e38dc3106e161f6213487b03af6251dfbac
Daniel Kuznetsov <85589967+DanilKuznet1@users.noreply.github.com>
2022-04-17 11:04:34 -0700

    reduce method, max value using reduce, display balance

--- JS course/11-Arrays-Bankist/starter/script.js
+++ JS course/11-Arrays-Bankist/starter/script.js
@@ -81,6 +81,30 @@ const displayMovements = function (movements) {
 
 displayMovements(account1.movements);
 
+const calcDisplayBalance = function (movements) {
+  const balance = movements.reduce((acc, current) => {
+    return acc + current;
+  }, 0);
+
+  labelBalance.textContent = `${balance ?? '0'} EUR`;
+};
+
+calcDisplayBalance(account1.movements);
+
+const createUsernames = function (accounts) {
+  accounts.forEach(account => {
+    account.username = account.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+
+createUsernames(accounts);
+
+console.log(accounts);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -104,7 +128,48 @@ displayMovements(account1.movements);
 //   console.log(`${value}: ${value}`);
 // });
 
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+//Reduce method
+const balance = movements.reduce((total, current) => {
+  return total + current;
+}, 0);
+
+console.log(balance);
+
+//Maximum value
+const max = movements.reduce((acc, mov) => {
+  if (acc > mov) return acc;
+  else return mov;
+}, movements[0]);
+
+console.log(max);
+
+//Filter method
+// const deposits = movements.filter(mov => {
+//   return mov > 0;
+// });
+
+// const withdrawals = movements.filter(mov => {
+//   return Math.abs(mov < 0);
+// });
+
+// console.log(withdrawals);
+
+// //Map method
+// const eurToUsd = 1.1;
+// const movementUSD = movements.map(mov => {
+//   return Math.floor(mov * eurToUsd);
+// });
+// console.log(movementUSD);
+
+// const movementsDescriptions = movements.map((mov, i, arr) => {
+//   return `Movement ${i + 1}: you ${
+//     mov > 0 ? 'deposited' : 'withdrew'
+//   } $ ${Math.abs(Math.floor(mov * eurToUsd))}`;
+// });
+
+// console.log(movementsDescriptions);
 
 // //for..of loop
 // for (const [i, movement] of movements.entries()) {
@@ -155,3 +220,45 @@ displayMovements(account1.movements);
 
 // //getting a last arr method
 // console.log(movements.slice(-1)[0]);
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, and stored the data into an array (one array for each).
+For now, they are just interested in knowing whether a dog is an adult or a puppy. A dog is an adult if it is at least 3 years old, and it's a puppy
+if it's less than 3 years old.
+
+Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the following things:
+
+1. Julia found out that the owners of the FIRST and the LAST TWO dogs actually have cats, not dogs! So create a shallow copy of Julia's array, and remove
+the cat ages from that copied array (because it's a bad practice to mutate function parameters)
+2. Create an array with both Julia's (corrected) and Kate's data
+3. For each remaining dog, log to the console whether it's an adult ("Dog number 1 is an adult, and is 5 years old")
+or a puppy ("Dog number 2 is still a puppy 🐶")
+4. Run the function for both test datasets
+
+HINT: Use tools from all lectures in this section so far 😉
+
+TEST DATA 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+// const checkDogs = function (dogsJulia, dogsKate) {
+//   const correctedDogsJulia = dogsJulia.splice(1, dogsJulia.length - 2);
+
+//   const dogs = [...correctedDogsJulia, ...dogsKate];
+//   // const dogs = correctedDogsJulia.concat(dogsKate);
+//   // return dogs;
+
+//   dogs.forEach(function (dog, i) {
+//     console.log(
+//       `Dog number ${i + 1} is ${
+//         dog > 3 ? 'an adult' : 'a puppy'
+//       }, and ${dog} years old`
+//     );
+//   });
+// };
+
+// console.log(checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]));
