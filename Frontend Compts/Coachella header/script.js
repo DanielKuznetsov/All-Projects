fc1133d4bbc89a5f27c8cff7ba012eabb1b1e574
Daniel Kuznetsov <85589967+DanilKuznet1@users.noreply.github.com>
2022-04-28 22:41:30 -0700

    fixed funcitonality of the dropdown - onmouseleave instead
    also, I added links functionality

--- Frontend Compts/Coachella header/script.js
+++ Frontend Compts/Coachella header/script.js
@@ -3,25 +3,24 @@ const dropdown = document.querySelector(".dropdown");
 const container = document.querySelector(".container");
 const navLogo = document.querySelector(".nav__logo");
 const nav = document.querySelector(".nav");
+const linkLists = document.querySelectorAll(".dropdown__list");
+const navImgs = document.querySelectorAll(".nav__img");
 
-// const removeDropDown = function (el) {
-//   el.addEventListener("mouseout", function () {
-//     dropdown.classList.remove("dropdown__animation");
-//     dropdown.classList.add("ani");
-//     container.classList.remove("container__after");
-//   });
-// };
-
-const getDropDown = function (el) {
-  el.addEventListener("mouseover", function () {
+links.forEach((link, i) => {
+  link.addEventListener("mouseover", function () {
     dropdown.classList.add("dropdown__animation");
     container.classList.add("container__after");
 
-    // if (dropdown.classList.contains("dropdown__animation")) {
-    //   const dropBlock = document.querySelector(".dropdown__animation");
+    linkLists.forEach((list) => {
+      list.style.display = "none";
+    });
 
-    //   removeDropDown(dropBlock);
-    // }
+    navImgs.forEach((list) => {
+      list.style.display = "none";
+    });
+
+    linkLists[i].style.display = "flex";
+    navImgs[i].style.display = "flex";
 
     const dropBlock = document.querySelector(".dropdown__animation");
     dropBlock.onmouseleave = () => {
@@ -30,8 +29,4 @@ const getDropDown = function (el) {
       container.classList.remove("container__after");
     };
   });
-};
-
-links.forEach((link) => {
-  getDropDown(link);
 });
