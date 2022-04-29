b2a73844f16ef94c0e33665de2f68fed60b95e61
Daniel Kuznetsov <85589967+DanilKuznet1@users.noreply.github.com>
2022-04-28 20:48:41 -0700

    fixed mouseout event

--- Frontend Compts/Coachella header/script.js
+++ Frontend Compts/Coachella header/script.js
@@ -4,33 +4,34 @@ const container = document.querySelector(".container");
 const navLogo = document.querySelector(".nav__logo");
 const nav = document.querySelector(".nav");
 
-const removeDropDown = function (el) {
-  el.addEventListener("mouseout", function () {
-    dropdown.classList.remove("dropdown__animation");
-    dropdown.classList.add("ani");
-    container.classList.remove("container__after");
-  });
-};
+// const removeDropDown = function (el) {
+//   el.addEventListener("mouseout", function () {
+//     dropdown.classList.remove("dropdown__animation");
+//     dropdown.classList.add("ani");
+//     container.classList.remove("container__after");
+//   });
+// };
 
 const getDropDown = function (el) {
-  el.addEventListener("mouseover", function (e) {
-    e.preventDefault();
-
+  el.addEventListener("mouseover", function () {
     dropdown.classList.add("dropdown__animation");
     container.classList.add("container__after");
 
-    if (dropdown.classList.contains("dropdown__animation")) {
-      const dropBlock = document.querySelector(".dropdown__animation");
+    // if (dropdown.classList.contains("dropdown__animation")) {
+    //   const dropBlock = document.querySelector(".dropdown__animation");
 
-      removeDropDown(dropBlock);
-    }
+    //   removeDropDown(dropBlock);
+    // }
+
+    const dropBlock = document.querySelector(".dropdown__animation");
+    dropBlock.onmouseleave = () => {
+      dropdown.classList.remove("dropdown__animation");
+      dropdown.classList.add("ani");
+      container.classList.remove("container__after");
+    };
   });
 };
 
 links.forEach((link) => {
   getDropDown(link);
 });
-
-if (dropdown.classList.contains("dropdown__animation")) {
-  getDropDown(nav);
-}
