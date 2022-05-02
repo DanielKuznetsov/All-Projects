cdea7d26dc1ee0c340091cf1720b6d991ee09070
Daniel Kuznetsov <85589967+DanilKuznet1@users.noreply.github.com>
2022-05-02 15:07:54 -0700

    scrollto()

--- JS course/13-Advanced-DOM-Bankist/starter/script.js
+++ JS course/13-Advanced-DOM-Bankist/starter/script.js
@@ -81,3 +81,38 @@ console.log(link.getAttribute('href'));
 console.log(logo.dataset.versionNumber);
 logo.setAttribute('data-version-beta', '2.0');
 console.log(logo.dataset.versionBeta);
+
+//Smooth scrolling
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  console.log(
+    `Current scroll (X/Y): ${window.pageXOffset}, ${window.pageYOffset}`
+  );
+
+  // console.log(
+  //   'height/width of the viewport',
+  //   document.documentElement.clientHeight,
+  //   document.documentElement.clientWidth
+  // );
+
+  //Scrolling
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  window.scrollTo({
+    left: s1coords.left + window.pageXOffset,
+    top: s1coords.top + window.pageYOffset,
+    behavior: 'smooth',
+  });
+
+  // section1.scrollIntoView({
+  //   behavior: 'smooth',
+  // });
+});
