e0c22109c9f839625b2e15af7be2aaee5626bf2b
Daniel Kuznetsov <85589967+DanilKuznet1@users.noreply.github.com>
2022-05-04 13:12:18 -0700

    callbacks and promises

--- JS course/14-OOP/starter/script.js
+++ JS course/14-OOP/starter/script.js
@@ -32,3 +32,58 @@ const car1 = new Car('BMW', 120);
 const car2 = new Car('BMW', 95);
 car1.accelerate();
 car1.brake();
+
+//ES6 Classes
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+
+  calcAge() {
+    return 2022 - this.birthYear;
+  }
+
+  get age() {
+    return 2024 - this.birthYear;
+  }
+
+  //Set a property that already exists
+  set fullName(name) {
+    if (name.includes(' ')) this._fullName = name;
+    else alert(`${name} is not a full name!`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
+}
+
+const jessica = new PersonCl('Jessica Davis', 1998);
+
+PersonCl.prototype.greet = function () {
+  console.log(`Hello, ${this.fullName}!`);
+};
+
+jessica.greet();
+
+const walter = new PersonCl('Walte r', 1990);
+
+const account = {
+  owner: 'Daniel',
+  movements: [200, 530, 120, 300],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+console.log(account.latest);
+account.latest = 50;
+console.log(account.movements);
+
+//Static methods
--- JS practice/Practice/starter/script.js
+++ JS practice/Practice/starter/script.js
@@ -1,12 +1,85 @@
-var twoSum = function (nums, target) {
-  for (let i = 0; i < nums.length; i++) {
-    const x = target - nums[i];
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[j] === x) {
-        return [i, j];
-      }
-    }
-  }
+// var twoSum = function (nums, target) {
+//   for (let i = 0; i < nums.length; i++) {
+//     const x = target - nums[i];
+//     for (let j = i + 1; j < nums.length; j++) {
+//       if (nums[j] === x) {
+//         return [i, j];
+//       }
+//     }
+//   }
+// };
+
+// console.log(twoSum([2, 3, 7, 4, 4, 2, 1], 8));
+
+const temperatures = [10, 5, 3];
+
+const sumTemperatures = (temperatures, callback) => {
+  // setTimeout to simulate an "expensive calculation"
+  setTimeout(() => {
+    const sum = temperatures.reduce((total, current) => total + current, 0);
+    callback(sum); // call the success callback (with the result of the sum)
+  }, 1000);
 };
 
-console.log(twoSum([3, 2, 4], 6));
+sumTemperatures(temperatures, (value) => {
+  console.log(value); // 18 (the sum of temperatures)
+});
+
+const welcomeUser = function (name, callback) {
+  setTimeout(() => {
+    console.log(`Welcome ${name}`);
+    callback();
+  }, 1000);
+};
+
+welcomeUser("Daniel", () => {
+  console.log(`Done processing`);
+});
+
+//---------------------------------------
+//---------------------------------------
+//---------------------------------------
+function clean(item) {
+  console.log(`I just cleaned a ${item}`);
+}
+
+var nails = ["rusty nail", "rusty nail", "bent nail", "clean nail"];
+var planks = ["splintered plank", "straight plank", "bent plank"];
+
+//CALLBACK FUNCTIONS
+function groupAndClean(items, cleaningMethod, done) {
+  for (var i = 0; i < items.length; i++) {
+    cleaningMethod(items[i]);
+  }
+
+  setTimeout(() => {
+    done();
+  }, 1000);
+}
+
+const done = () => console.log("Whew, that was a lot of work!");
+
+// groupAndClean(nails.concat(planks), clean, done);
+
+//REFACTORED USING PROMISE
+const groupClean = (items, cleaningMethod) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      for (var i = 0; i < items.length; i++) {
+        cleaningMethod(items[i]);
+      }
+      resolve();
+    }, 2500);
+    // reject("Items were not found.");
+  });
+};
+
+groupClean(nails.concat(planks), clean)
+  .then(() => {
+    setTimeout(() => {
+      console.log("Whew, that was a lot of work!");
+    }, 5000);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
