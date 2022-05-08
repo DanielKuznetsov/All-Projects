06d404ce727b0c66b7376832497393894d6a2937
Daniel Kuznetsov <85589967+DanilKuznet1@users.noreply.github.com>
2022-05-08 15:40:28 -0700

    refactored code

--- Frontend Compts/Rivian capability/script.js
+++ Frontend Compts/Rivian capability/script.js
@@ -3,19 +3,29 @@
 const nav = document.querySelector(".nav");
 const contentTabs = document.querySelectorAll(".content__wrapper");
 const navBtns = document.querySelectorAll(".nav__btn");
+const navItems = document.querySelectorAll(".nav__item");
+
+const removeClass = function (items, clName) {
+  items.forEach((item) => item.classList.remove(clName));
+};
+
+const addClass = function (selectedClass, addedClass) {
+  document.querySelector(selectedClass).classList.add(addedClass);
+};
 
 nav.addEventListener("click", function (e) {
   const clicked = e.target.closest(".nav__item");
 
   if (!clicked) return;
 
-  navBtns.forEach((btn) => btn.classList.remove("nav__btn--active"));
-  document
-    .querySelector(`.nav__btn--${clicked.dataset.tab}`)
-    .classList.add("nav__btn--active");
+  removeClass(navBtns, "nav__btn--active");
+  removeClass(navItems, "nav__item--active");
+  removeClass(contentTabs, "content__wrapper--active");
 
-  contentTabs.forEach((c) => c.classList.remove("content__wrapper--active"));
-  document
-    .querySelector(`.content__wrapper--${clicked.dataset.tab}`)
-    .classList.add("content__wrapper--active");
+  addClass(
+    `.content__wrapper--${clicked.dataset.tab}`,
+    "content__wrapper--active"
+  );
+  addClass(`.nav__btn--${clicked.dataset.tab}`, "nav__btn--active");
+  addClass(`.nav__item--${clicked.dataset.tab}`, "nav__item--active");
 });
