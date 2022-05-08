e0138710df08d5694fdbbad16e178b2825133c91
Daniel Kuznetsov <85589967+DanilKuznet1@users.noreply.github.com>
2022-05-08 15:17:02 -0700

    functionality of component's content div is done

--- Frontend Compts/Rivian capability/script.js
+++ Frontend Compts/Rivian capability/script.js
@@ -1,1 +1,21 @@
 "use strict";
+
+const nav = document.querySelector(".nav");
+const contentTabs = document.querySelectorAll(".content__wrapper");
+const navBtns = document.querySelectorAll(".nav__btn");
+
+nav.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".nav__item");
+
+  if (!clicked) return;
+
+  navBtns.forEach((btn) => btn.classList.remove("nav__btn--active"));
+  document
+    .querySelector(`.nav__btn--${clicked.dataset.tab}`)
+    .classList.add("nav__btn--active");
+
+  contentTabs.forEach((c) => c.classList.remove("content__wrapper--active"));
+  document
+    .querySelector(`.content__wrapper--${clicked.dataset.tab}`)
+    .classList.add("content__wrapper--active");
+});
