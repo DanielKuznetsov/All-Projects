a9bf373ce3db405dc2c5d2ec092092d81a2af8fa
Daniel Kuznetsov <85589967+DanilKuznet1@users.noreply.github.com>
2022-05-16 12:17:36 -0700

    learned that i can add classes to :root CSS variables and that i also can work with them

--- Frontend Compts/Light:black:dark theme/main.js
+++ Frontend Compts/Light:black:dark theme/main.js
@@ -1,8 +1,16 @@
-const span = document.querySelector("span");
-const light = document.querySelector(".switch1");
-const container = document.querySelector(".container");
+const movPart = document.querySelector(".switch__box--after");
+const btns = document.querySelectorAll(".btn");
+const root = document.querySelector(":root");
 
-light.addEventListener("click", function () {
-  span.style.color = "#191919";
-  container.style.backgroundColor = "#fff";
+btns.forEach((btn, i) => {
+  btn.addEventListener("click", function () {
+    movPart.style.transform = `translateX(${100 * i}%)`;
+
+    root.classList.remove("black-theme");
+    root.classList.remove("dark-theme");
+
+    const styles = [, "dark-theme", "black-theme"];
+
+    root.classList.add(`${styles[i]}`);
+  });
 });
