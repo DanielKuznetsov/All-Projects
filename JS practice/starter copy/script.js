c035be1967eb6ed3cf8e1e13e174863441a31cf3
Daniel Kuznetsov <85589967+DanilKuznet1@users.noreply.github.com>
2022-05-20 09:20:35 -0700

    todos functionality is done

--- JS practice/starter copy/script.js
+++ JS practice/starter copy/script.js
@@ -1,38 +1,41 @@
-class Robot {
-  constructor(age, name, city) {
-    this.age = age;
-    this.name = name;
-    this.city = city;
+const form = document.querySelector("#form");
+const input = document.querySelector("#input");
+const todosUl = document.querySelector("#todos");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  addTodo();
+});
+
+function addTodo(todo) {
+  let todoText = input.value;
+
+  if (todo) {
+    todoText = todo;
   }
 
-  introduceSelf() {
-    return `My name is ${this.name}, I'm ${this.age} years old and live in ${this.city}`;
+  if (todoText) {
+    const todoEl = document.createElement("li");
+
+    if (todo && todo.completed) {
+      todoEl.classList.add("completed");
+    }
+
+    todoEl.innerText = todoText;
+
+    todoEl.addEventListener("click", () =>
+      todoEl.classList.toggle("completed")
+    );
+
+    todoEl.addEventListener("contextmenu", (e) => {
+      e.preventDefault();
+
+      todoEl.remove();
+    });
+
+    todosUl.appendChild(todoEl);
+
+    input.value = "";
   }
 }
-
-// Will come from inputs or API later on
-const nameC = "Daniel";
-const ageC = 24;
-const cityC = "Seal Beach";
-
-const nameC2 = "Robert";
-const ageC2 = 31;
-const cityC2 = "Long Beach";
-
-const r1 = new Robot(ageC, nameC, cityC);
-const r2 = new Robot(ageC, nameC2, cityC2);
-
-const arrRobots = [];
-arrRobots.push(r1, r2);
-
-class Person {
-  constructor(name, personality, isSitting, robotOwned) {
-    this.name = name;
-    this.personality = personality;
-    this.isSitting = isSitting;
-    this.robotOwned = robotOwned;
-  }
-}
-
-const p1 = new Person("Daniel", "talkative", true, arrRobots);
-console.log(p1);
