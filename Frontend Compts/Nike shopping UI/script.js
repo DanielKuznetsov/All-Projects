8dab274897581839e8509214348859213c3fb2d4
Daniel Kuznetsov <85589967+DanilKuznet1@users.noreply.github.com>
2022-06-07 13:35:03 -0700

    implemented add to cart btn functionality

--- Frontend Compts/Nike shopping UI/script.js
+++ Frontend Compts/Nike shopping UI/script.js
@@ -1,39 +1,43 @@
-const pickTab = document.querySelector(".picking");
-const cart = document.querySelector(".cart");
+class UI {
+  static readShoes() {
+    const shoes = [
+      {
+        bgColor: "grayish",
+        shoeImg:
+          "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1315882/air-zoom-pegasus-36-mens-running-shoe-wide-D24Mcz-removebg-preview.png",
+        shoeName: "Nike Air Zoom Pegasus 36",
+        shoeText: `The iconic Nike Air Zoom Pegasus 36 offers more cooling and mesh
+      that targets breathability across high-heat areas. A slimmer
+      heel collar and tongue reduce bulk, while exposed cables give
+      you a snug fit at higher speeds.`,
+        shoePrice: "108.97",
+      },
+      {
+        bgColor: "purpleish",
+        shoeImg:
+          "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1315882/air-zoom-pegasus-36-shield-mens-running-shoe-24FBGb__1_-removebg-preview.png",
+        shoeName: "Nike Air Zoom Pegasus 36 Shield",
+        shoeText: `The Nike Air Zoom Pegasus 36 Shield gets updated to conquer wet routes. A water-repellent upper combines with an outsole that helps create grip on wet surfaces, letting you run in confidence despite the weather.`,
+        shoePrice: "89.97",
+      },
+      {
+        bgColor: "yellowish",
+        shoeImg:
+          "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1315882/cruzrone-unisex-shoe-T2rRwS-removebg-preview.png",
+        shoeName: "Nike Cruz One",
+        shoeText: `Designed for steady, easy-paced movement, the Nike CruzrOne keeps you going. Its rocker-shaped sole and plush, lightweight cushioning let you move naturally and comfortably. The padded collar is lined with soft wool, adding luxury to every step, while mesh details let your foot breathe. There’s no finish line—there’s only you, one step after the next.`,
+        shoePrice: "100.97",
+      },
+    ];
 
-const shoes = [
-  {
-    bgColor: "grayish",
-    shoeImg:
-      "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1315882/air-zoom-pegasus-36-mens-running-shoe-wide-D24Mcz-removebg-preview.png",
-    shoeName: "Nike Air Zoom Pegasus 36",
-    shoeText: `The iconic Nike Air Zoom Pegasus 36 offers more cooling and mesh
-  that targets breathability across high-heat areas. A slimmer
-  heel collar and tongue reduce bulk, while exposed cables give
-  you a snug fit at higher speeds.`,
-    shoePrice: "108.97",
-  },
-  {
-    bgColor: "purpleish",
-    shoeImg:
-      "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1315882/air-zoom-pegasus-36-shield-mens-running-shoe-24FBGb__1_-removebg-preview.png",
-    shoeName: "Nike Air Zoom Pegasus 36 Shield",
-    shoeText: `The Nike Air Zoom Pegasus 36 Shield gets updated to conquer wet routes. A water-repellent upper combines with an outsole that helps create grip on wet surfaces, letting you run in confidence despite the weather.`,
-    shoePrice: "89.97",
-  },
-  {
-    bgColor: "yellowish",
-    shoeImg:
-      "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1315882/cruzrone-unisex-shoe-T2rRwS-removebg-preview.png",
-    shoeName: "Nike Cruz One",
-    shoeText: `Designed for steady, easy-paced movement, the Nike CruzrOne keeps you going. Its rocker-shaped sole and plush, lightweight cushioning let you move naturally and comfortably. The padded collar is lined with soft wool, adding luxury to every step, while mesh details let your foot breathe. There’s no finish line—there’s only you, one step after the next.`,
-    shoePrice: "100.97",
-  },
-];
+    shoes.forEach((shoe) => UI.displayShoes(shoe));
+    // shoes.forEach((shoe) => UI.displayCart(shoe));
+  }
 
-const createShoes = function (shoes) {
-  shoes.forEach((shoe, i) => {
-    const item = `<div class="shoe">
+  static displayShoes(shoe) {
+    const cardTitle = document.querySelector(".card-title");
+
+    const el = `<div class="shoe">
     <div class="shoe__img-wrapper ${shoe.bgColor}">
       <img
         src=${shoe.shoeImg}
@@ -49,85 +53,73 @@ const createShoes = function (shoes) {
     </div>
     <div class="shoe__activity">
       <span class="shoe__price">$${shoe.shoePrice}</span>
-      <div class="shoe__add-to-cart btn-cart" data-item="${i}">add to cart</div>
+      <div class="shoe__add-to-cart btn-cart">add to cart</div>
     </div>
     </div>
     `;
 
-    pickTab.insertAdjacentHTML("beforeend", item);
-  });
-};
-
-const addToCart = function (items, incr = 1) {
-  const btnsCart = document.querySelectorAll(".btn-cart");
-
-  btnsCart.forEach((btnCart, i) => {
-    btnCart.addEventListener("click", function (e) {
-      const item = items[e.target.dataset.item];
-      let incr = 1;
-
-      const cartShoe = `
-      <div class="card-item">
-      <div class="card-item__left">
-        <div class="img-wrapper">
-          <img
-            src=${item.shoeImg}
-            alt="Air Zoom"
-          />
-        </div>
-      </div>
-      <div class="card-item__right">
-        <p class="title">${item.shoeName}</p>
-        <p class="price">${item.shoePrice}</p>
-        <div class="incr-tab">
-          <span class="icon-wrapper icon-back"
-            ><ion-icon class="icon" name="chevron-back-outline"></ion-icon
-          ></span>
-          <p class="incr-text" data-item-num="${i}">${incr}</p>
-          <span class="icon-wrapper icon-forward"
-            ><ion-icon
-              class="icon"
-              name="chevron-forward-outline"
-            ></ion-icon
-          ></span>
-        </div>
-      </div>
-    </div>`;
-
-      cart.insertAdjacentHTML("beforeend", cartShoe);
-
-      const children = [...cart.children];
-      children.forEach((child) => {});
-
-      const tabForward = document.querySelector(".icon-forward");
-      const tabBack = document.querySelector(".icon-back");
-      const incrText = document.querySelector(".incr-text");
-
-      incrTab(tabForward, tabBack, incrText, incr, children);
-
-      removeCartTitle(cart);
-    });
-  });
-};
-
-createShoes(shoes);
-addToCart(shoes);
-
-const removeCartTitle = function (cartParent) {
-  if (cartParent.children.length > 2) {
-    document.querySelector(".subtitle-cart").style.display = "none";
+    cardTitle.insertAdjacentHTML("afterend", el);
   }
-};
 
-const incrTab = function (btnForward, btnBack, textTab, incr, children) {
-  btnForward.addEventListener("click", function () {
-    incr++;
-    textTab.textContent = incr;
-  });
-  btnBack.addEventListener("click", function () {
-    incr--;
-    textTab.textContent = incr;
-    // if (incr === 0) {
-    // }
-  });
-};
+  static displayCart(shoe) {
+    const cart = document.querySelector(".cart");
+
+    const cartShoe = `
+    <div class="card-item">
+    <div class="card-item__left">
+      <div class="img-wrapper">
+        <img
+          src=${shoe.photo}
+          alt="Air Zoom"
+        />
+      </div>
+    </div>
+    <div class="card-item__right">
+      <p class="title">${shoe.name}</p>
+      <p class="price">${shoe.price}</p>
+      <div class="incr-tab">
+        <span class="icon-wrapper icon-back"
+          ><ion-icon class="icon" name="chevron-back-outline"></ion-icon
+        ></span>
+        <p class="incr-text">1</p>
+        <span class="icon-wrapper icon-forward"
+          ><ion-icon
+            class="icon"
+            name="chevron-forward-outline"
+          ></ion-icon
+        ></span>
+      </div>
+    </div>
+  </div>`;
+
+    cart.insertAdjacentHTML("beforeend", cartShoe);
+  }
+}
+
+class Cart {
+  constructor(photo, name, price) {
+    this.photo = photo;
+    this.name = name;
+    this.price = price;
+  }
+}
+
+// Event: Get Shoe
+document.querySelector(".picking").addEventListener("click", (e) => {
+  if (e.target.classList.contains("btn-cart")) {
+    const photo =
+      e.target.parentElement.parentElement.children[0].children[0].src;
+    const name =
+      e.target.parentElement.previousElementSibling.children[0].textContent;
+    const price = e.target.previousElementSibling.textContent;
+
+    const shoeItem = new Cart(photo, name, price);
+    UI.displayCart(shoeItem);
+  }
+});
+
+// Event: Increment Number of Shoes
+
+// Remove "Cart is empty" text
+
+document.addEventListener("DOMContentLoaded", UI.readShoes);
