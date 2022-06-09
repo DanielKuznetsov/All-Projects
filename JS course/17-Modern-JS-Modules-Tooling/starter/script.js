624480927ea1210b37a0bffe853b31b55737c28f
Daniel Kuznetsov <85589967+DanilKuznet1@users.noreply.github.com>
2022-06-09 11:19:43 -0700

    top level await is not needed to be used with async keyword but will block execution of the rest of the code

--- JS course/17-Modern-JS-Modules-Tooling/starter/script.js
+++ JS course/17-Modern-JS-Modules-Tooling/starter/script.js
@@ -7,16 +7,43 @@
 // import { toolPrice as price, qt } from './script2.js';
 // console.log(price, qt);
 
-// Importing Everything
+// ---- Importing Everything
 // import * as ShoppingCart from './script2.js';
 
 // ShoppingCart.addToCart('cheese', 2);
 // ShoppingCart.addToCart('bread', 5);
 
-// Importing Default Value = always returnes a "RETURN" value and
-// we can change the name of the variable or function we are expoering
+// ---- Importing Default Value = always returnes a "RETURN" value and
+// ---- we can change the name of the variable or function we are expoering
 import add, { toolPrice as price, qt, toolPrice, cart } from './script2.js';
 add('pizza', 3);
 add('bread', 5);
 add('apples', 7);
-console.log(cart);
+// console.log(cart);
+
+// ---- Top Level "Await" - works without async in modules since 2022
+// ---- Using top level await will stop execution of the rest of the code
+// ---- that's underneath the await keyword - use with caution
+// console.log('Start fetching...');
+
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await res.json();
+// console.log(data);
+
+// console.log('Something');
+
+const getLastPost = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+  // console.log(data);
+
+  // will always return a promise because data did not fetched yet
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+// Not Clean
+const lastPost = getLastPost();
+lastPost.then(last => console.log(last));
+
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
--- JS course/17-Modern-JS-Modules-Tooling/starter/script2.js
+++ JS course/17-Modern-JS-Modules-Tooling/starter/script2.js
@@ -1,6 +1,11 @@
 export const shoppingCart = 10;
 const cart = [];
 
+// Blocking code
+console.log('Start fetching users..');
+await fetch('https://jsonplaceholder.typicode.com/users');
+console.log('Fetching fetching users');
+
 export { cart };
 
 export const addToCart = function (product, quantity) {
@@ -17,6 +22,6 @@ export { toolPrice, totalQuantity as qt };
 // Default Export
 export default function (product, quantity) {
   cart.push({ product, quantity });
-  console.log(`${quantity} ${product} was added to the cart`);
+  // console.log(`${quantity} ${product} was added to the cart`);
   // console.log(cart);
 }
