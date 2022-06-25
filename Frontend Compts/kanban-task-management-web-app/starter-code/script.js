b925e9c7c69a979e7cdde1562ed511e2b026266e
Daniel Kuznetsov <85589967+DanilKuznet1@users.noreply.github.com>
2022-06-25 11:48:52 -0700

    created a function that correct height of the app
    so no matter what the height of the or logo is, the main height is still going to be 100vh

--- Frontend Compts/kanban-task-management-web-app/starter-code/script.js
+++ Frontend Compts/kanban-task-management-web-app/starter-code/script.js
@@ -1,6 +1,12 @@
-const logoElHeight = document.querySelector(".logo").offsetHeight;
-const naviagtionBody = document.querySelector(".navigation__body");
+const logoEl = document.querySelector(".logo");
+const navigationBody = document.querySelector(".navigation__body");
+const navbarEl = document.querySelector(".navbar");
+const app = document.querySelector(".app");
 
-console.log(logoElHeight / 10);
+const fixAppHeight = (elOffset, element) => {
+  elHeight = elOffset.offsetHeight;
+  element.style.height = `calc(100% - ${elHeight / 10}rem)`;
+};
 
-naviagtionBody.style.height = `calc(100% - ${logoElHeight / 10}rem)`;
+fixAppHeight(logoEl, navigationBody);
+fixAppHeight(navbarEl, app);
