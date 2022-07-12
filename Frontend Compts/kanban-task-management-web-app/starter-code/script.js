5079f4fc3398a1e3615769aa1ee71374774be331
Daniel Kuznetsov <85589967+DanilKuznet1@users.noreply.github.com>
2022-07-11 18:54:24 -0700

    implemented functionality of -Add New Task- button. I also created a helper function for an outside click

--- Frontend Compts/kanban-task-management-web-app/starter-code/script.js
+++ Frontend Compts/kanban-task-management-web-app/starter-code/script.js
@@ -21,3 +21,38 @@ document.querySelector(".see-nav-btn").addEventListener("click", () => {
   document.querySelector(".navigation").style.display = "block";
   document.querySelector(".see-nav-btn").style.display = "none";
 });
+
+let boards = [];
+
+function hideAddBoardBtnDiv() {
+  boards.length > 0 && app.classList.add("app-empty__none");
+}
+
+const addBoardBtn = document.querySelector(".add-board-btn");
+
+function handleAddBoardClick() {
+  document.querySelector(".add-board-btn").addEventListener("click", () => {
+    document.querySelector(".new-column").style.display = "block";
+  });
+}
+
+function addNewBoard() {
+  const columnNameInput = document.querySelector("#column-name");
+  const addColumnBtn = document.querySelector(".new-column__btn");
+  const outsideDiv = document.querySelector(".new-column");
+  clickOutsideDiv(outsideDiv);
+
+  addColumnBtn.addEventListener("click", () => {});
+}
+
+function clickOutsideDiv(div) {
+  div.addEventListener("click", (e) => {
+    if (e.target === div) {
+      div.style.display = "none";
+    }
+  });
+}
+
+addNewBoard();
+handleAddBoardClick();
+hideAddBoardBtnDiv();
